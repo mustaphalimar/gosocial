e682c43e2d9daee5386dd21b13749806dace2c2c
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-03-15 17:22:06 +0000

    added User.Create method

--- internal/store/users.go
+++ internal/store/users.go
@@ -9,7 +9,7 @@ type User struct {
 	ID        int64  `json:"id"`
 	Username  string `json:"username"`
 	Email     string `json:"email"`
-	Password  string `json:"password"`
+	Password  string `json:"-"`
 	CreatedAt string `json:"created_at"`
 }
 
@@ -17,6 +17,20 @@ type UsersStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error {
+func (s *UsersStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users(username,email,password)
+		VALUES ($1,$2,$3)
+		RETURNING id,email,created_at
+	`
+	err := s.db.QueryRowContext(ctx, query,
+		user.Username,
+		user.Email,
+		user.Password,
+	).Scan(&user.ID, &user.Email, &user.CreatedAt)
+
+	if err != nil {
+		return err
+	}
 	return nil
 }
