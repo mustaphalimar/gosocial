8fffb9423b31e56df9036375c741101b2213bcd9
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-03-15 15:57:17 +0000

    bootsraping api, added go-chi router & middlewares

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 type application struct {
 	config config
@@ -10,11 +17,35 @@ type config struct {
 	addr string
 }
 
-func (app *application) run() error {
-	mux := http.NewServeMux()
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// middlewares
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	// routers
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
+}
+
+func (app *application) run(mux http.Handler) error {
+
 	srv := &http.Server{
-		Addr:    app.config.addr,
-		Handler: mux,
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
 	}
+
+	log.Printf("Server has started at %s", app.config.addr)
+
 	return srv.ListenAndServe()
 }
