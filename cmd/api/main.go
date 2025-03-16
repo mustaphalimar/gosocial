d92490f68db1c20cc909a196b9d746a14f4ecd2b
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-03-16 01:12:56 +0000

    added docker-compose

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/joho/godotenv"
+	"github.com/mustaphalimar/go-social/internal/db"
 	"github.com/mustaphalimar/go-social/internal/env"
 	"github.com/mustaphalimar/go-social/internal/store"
 )
@@ -16,9 +17,21 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr: env.GetString("DATABASE_URL", "postgresql://admin:admin@localhost/go-social?sslmode=disable"),
+			// limit number of open connection to the db from our API connection pool
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
