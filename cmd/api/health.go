172cb7e2129c399877f080d856693b222e99d4fd
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-03-17 03:05:55 +0000

    added json encoder & decoder

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,11 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
-func (app *application) healthCheckHandler(
-	w http.ResponseWriter,
-	r *http.Request,
-) {
-	w.Write([]byte("Ok"))
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":  "OK",
+		"data":    "posts[]",
+		"env":     app.config.env,
+		"version": version,
+	}
+
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		// throw err
+		log.Println(err.Error())
+	}
 
 }
---
+++ cmd/api/json.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func writeJSON(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func readJSON(w http.ResponseWriter, request *http.Request, data any) error {
+	decoder := json.NewDecoder(request.Body)
+	return decoder.Decode(data)
+}
