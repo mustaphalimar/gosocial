79e27624ad90077bf9ffa65fb0100a40a03d5bf0
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-03-18 05:13:57 +0000

    added create post validation using go-playground/validator package

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -10,8 +10,8 @@ import (
 )
 
 type CreatePostPayload struct {
-	Title   string   `json:"title"`
-	Content string   `json:"content"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags"`
 }
 
@@ -19,6 +19,11 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	var postPatyload CreatePostPayload
 
 	if err := readJSON(w, r, &postPatyload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(postPatyload); err != nil {
 		app.badRequestError(w, r, err)
 		return
 	}
