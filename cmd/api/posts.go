74076f27b93ff0aa7d8e01d3ed4d9a579aea68b2
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-03-19 05:40:13 +0000

    standardized json responses

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -45,7 +45,7 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusCreated, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
@@ -62,7 +62,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 
 	post.Comments = comments
 
-	if err := writeJSON(w, http.StatusOK, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 	}
 }
@@ -99,7 +99,7 @@ func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request
 		app.internalServerError(w, r, err)
 	}
 
-	if err = writeJSON(w, http.StatusOK, post); err != nil {
+	if err = app.jsonResponse(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
