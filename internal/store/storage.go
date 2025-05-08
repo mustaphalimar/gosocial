67bc2e6646102fe638e2ecb0c5f9c52ef3a35076
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-05-08 23:20:37 +0100

    added withTx storage func

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -47,3 +47,16 @@ func NewStorage(db *sql.DB) Storage {
 		Followers: &FollowerStore{db},
 	}
 }
+
+func withTx(db *sql.DB, ctx context.Context, fn func(*sql.Tx) error) error {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := fn(tx); err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
