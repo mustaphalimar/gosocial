762d7c1d443dc351fc7a4a03c1ca71f16ba66ad2
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-05-15 22:13:37 +0100

    added cors config

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/mustaphalimar/go-social/docs"
 	"github.com/mustaphalimar/go-social/internal/mailer"
 	"github.com/mustaphalimar/go-social/internal/store"
@@ -47,11 +48,23 @@ func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
 
 	// middlewares
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Logger)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
 
 	// routers
 	r.Route("/v1", func(r chi.Router) {
