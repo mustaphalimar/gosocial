9dfe661713b2099e8507b0ea3bc4c48466c1061d
Mustapha Limar <58789886+mustaphalimar@users.noreply.github.com>
2025-05-17 23:29:46 +0100

    auth package and jwt methods

---
+++ internal/auth/jwt.go
@@ -0,0 +1,31 @@
+package auth
+
+import "github.com/golang-jwt/jwt/v5"
+
+type JWTAuthenticator struct {
+	secret string
+	aud    string
+	iss    string
+}
+
+func NewJWTAuthenticator(secret, aud, iss string) JWTAuthenticator {
+	return JWTAuthenticator{
+		secret,
+		aud,
+		iss,
+	}
+}
+
+func (ja *JWTAuthenticator) GenerateToken(claims jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(ja.secret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, err
+}
+
+func (ja *JWTAuthenticator) ValidateToken(token string) (*jwt.Token, error) {
+	return nil, nil
+}
